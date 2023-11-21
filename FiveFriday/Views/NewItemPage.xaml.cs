8e8f849dcc28c5984231e4ed12a58dd3b380245c
JBillson <justin.billson@gmail.com>
2023-11-21 23:01:29 +0200

    project setup

---
+++ FiveFriday/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using FiveFriday.Models;
+using FiveFriday.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FiveFriday.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
